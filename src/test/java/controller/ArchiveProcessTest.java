becce024ffe47782f5fe91bb2e8eed3709aff4ea
Novoselov-pavel <Novosselov.pavel@gmail.com>
2020-02-20 00:35:17 +0300

    start writing - BashOptionRead - OK. ArchiveProcess - OK.

---
+++ src/test/java/controller/ArchiveProcessTest.java
@@ -0,0 +1,39 @@
+package controller;
+
+import controller.interfaces.FabricControllerInterface;
+import controller.interfacesImplementation.ArchiveProcess;
+import controller.interfacesImplementation.FabricController;
+import exception.InvalidBashOption;
+import model.Settings;
+import org.junit.jupiter.api.Test;
+
+import javax.naming.ldap.ControlFactory;
+import java.io.UnsupportedEncodingException;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ArchiveProcessTest {
+
+    @Test
+    void write() {
+        try {
+            FabricControllerInterface controllerInterface = createSetting();
+            controllerInterface.workFormSetting();
+            assertTrue(true);
+        } catch (Exception ex) {
+            fail();
+        }
+
+    }
+
+
+    private FabricControllerInterface createSetting() throws UnsupportedEncodingException, InvalidBashOption {
+        String[] input1= new String[3];
+        input1[0] = "-a";
+        input1[1] = "/home/pavel/IdeaProjects/npn/console/01_archivist/src/test/java/controller/TestInput/";
+        input1[2] = "/home/pavel/IdeaProjects/npn/console/01_archivist/src/test/java/controller/TestOutput/";
+        return new FabricController(input1);
+    }
+
+}
